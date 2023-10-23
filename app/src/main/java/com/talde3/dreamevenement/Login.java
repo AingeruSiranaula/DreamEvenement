bd11d3b8b7f4ce64651cb41b3873b32568b20945
in2dm3 <aingerudaniel.siranaulasa@elorrieta-errekamari.com>
2023-10-23 13:23:34 +0500

    Anonimous login-a, shared preferences

--- app/src/main/java/com/talde3/dreamevenement/Login.java
+++ app/src/main/java/com/talde3/dreamevenement/Login.java
@@ -5,7 +5,9 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +24,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    EditText email, pasahitza;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +32,16 @@ public class Login extends AppCompatActivity {
 
         Button btnLogin = findViewById(R.id.btnLogin);
         TextView txtErregistroa = findViewById(R.id.txtErregistroaLogin);
+        TextView txtAnonymousLogin = findViewById(R.id.txtAnonymousLogin);
 
-        EditText email = findViewById(R.id.etEmailLogin);
-        EditText pasahitza = findViewById(R.id.etPasahitzaLogin);
+        email = findViewById(R.id.etEmailLogin);
+        pasahitza = findViewById(R.id.etPasahitzaLogin);
 
         mAuth = FirebaseAuth.getInstance();
 
+        kargatuSharedPreferences();
+
         // Logeatze kasuan
-
-
         btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -55,6 +59,7 @@ public class Login extends AppCompatActivity {
                                         Log.d(TAG, "signInWithEmail:success");
                                         Intent intent = new Intent(Login.this, Etxea.class);
                                         startActivity(intent);
+                                        gordeSharedPreferences();
                                     } else {
                                         Log.w(TAG, "signInWithEmail:failure", task.getException());
                                         Toast.makeText(Login.this, "Contraseña o usuario incorrectos",
@@ -77,5 +82,48 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Login anonimoa
+        txtAnonymousLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInAnonymously()
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInAnonymously:success");
+                                    Intent intent = new Intent(Login.this, Etxea.class);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInAnonymously:failure", task.getException());
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+    private void kargatuSharedPreferences() {
+        SharedPreferences preferences = getSharedPreferences("credentials", Context.MODE_PRIVATE);
+
+        String user = preferences.getString("user", "");
+        email.setText(user);
+    }
+
+    private void gordeSharedPreferences() {
+        SharedPreferences preferences = getSharedPreferences("credentials", Context.MODE_PRIVATE);
+
+        String user = email.getText().toString();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user", user);
+
+        email.setText(user);
+
+        editor.commit();
     }
 }
